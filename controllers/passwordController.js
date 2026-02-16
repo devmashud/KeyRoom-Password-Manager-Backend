6b723cb4267364f42d408f7d1593f703d0ae49ab
devmashud <2345arham@gmail.com>
2026-02-16 22:44:35 +0600

    update api and updatepassword function add

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -21,3 +21,21 @@ export const getPassword = async(req, res)=>{
     res.json(items)
 }
 
+export const updatePassword = async (req, res) => {
+      console.log("UPDATE HIT", req.params.id, req.body);
+
+    const {id} = req.params;
+    const updated = await Password.findByIdAndUpdate(
+    id, 
+    req.body,
+    {returnDocument: "after"}
+    );
+
+    if(!updated){
+        return res.status(404).json({message: "Not Found"})
+    }
+    res.json(updated)
+    console.log(updated)
+
+}
+
--- routes/passwordRoutes.js
+++ routes/passwordRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createPassword, getPassword } from "../controllers/passwordController.js"
+import { createPassword, getPassword, updatePassword } from "../controllers/passwordController.js"
 
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post("/", createPassword)
 
 router.get("/", getPassword)
+
+router.put("/:id",updatePassword )
 
 export default router;
